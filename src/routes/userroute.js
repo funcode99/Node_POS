d5e1326e2e2b7e95dcdf07bd99d1f7a56ce5679d
Idris <50961526+funcode99@users.noreply.github.com>
2020-12-30 17:56:18 +0700

    Update userroute.js

--- src/routes/userroute.js
+++ src/routes/userroute.js
@@ -5,7 +5,7 @@ const Route = express.Router();
 const Validate = require("../middleware/validate");
 const Cache = require("../middleware/cache");
 
-Route.get("/", Validate.Admin, controller.getAll);
+Route.get("/", controller.getAll);
 Route.get("/", controller.getByUser);
 Route.post("/", controller.addUsers);
 Route.delete("/:id", controller.delUsers);
